3ad1808ef2dea398f9b338ca3ac5b1a97a24a8ca
Sasa Lotina <lotinasa@gmail.com>
2023-02-16 18:48:39 +0100

    Align network connector with updated network

--- src/pages/Root/App.js
+++ src/pages/Root/App.js
@@ -118,22 +118,24 @@ const App = () => {
                 console.log('after d networkId', networkId, 'client.lastUsedChainId ', client.lastUsedChainId);
                 console.log('signer', signer, isLoading);
             }
+
             try {
                 if (providerNetworkId && providerNetworkId !== networkId) {
                     dispatch(updateNetworkSettings({ networkId: providerNetworkId }));
+                    networkConnector.setNetworkSettings({
+                        networkId: providerNetworkId,
+                        provider:
+                            parseInt(window.ethereum?.chainId, 16) === providerNetworkId
+                                ? !!signer && !!signer.provider
+                                    ? new ethers.providers.Web3Provider(signer.provider.provider, 'any')
+                                    : window.ethereum
+                                    ? new ethers.providers.Web3Provider(window.ethereum, 'any')
+                                    : provider
+                                : provider,
+                        signer,
+                    });
                 }
-                networkConnector.setNetworkSettings({
-                    networkId: providerNetworkId,
-                    provider:
-                        parseInt(window.ethereum?.chainId, 16) === providerNetworkId
-                            ? !!signer && !!signer.provider
-                                ? new ethers.providers.Web3Provider(signer.provider.provider, 'any')
-                                : window.ethereum
-                                ? new ethers.providers.Web3Provider(window.ethereum, 'any')
-                                : provider
-                            : provider,
-                    signer,
-                });
+
                 dispatch(setAppReady());
             } catch (e) {
                 dispatch(setAppReady());
